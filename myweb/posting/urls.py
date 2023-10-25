bdf1365f99f0bd86f027704c80f4951d9988a472
Taufiqurrohman58 <148951044+Taufiqurrohman58@users.noreply.github.com>
2023-10-25 01:10:14 -0700

    Add files via upload

---
+++ myweb/posting/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('posting/', views.posting, name='posting'),
+    path('instagram', views.instagram, name='instagram'),
+]
